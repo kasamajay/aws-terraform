38282291c025e39641fdd2081d06438214975b4e
Ajay Kumar Kasam <kasamajay@gmail.com>
2021-03-06 21:19:35 +0000

    64bit x86 ec2 instance in london

--- terraform/instances/cds-london/providers.tf
+++ terraform/instances/cds-london/providers.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  version = ""
 }
 
 provider "vault" {
--- terraform/modules/nginx/main.tf
+++ terraform/modules/nginx/main.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "nginx" {
-  ami = ""
+  ami = "ami-0ffd774e02309201f"
   instance_type = var.instance_type
 }
